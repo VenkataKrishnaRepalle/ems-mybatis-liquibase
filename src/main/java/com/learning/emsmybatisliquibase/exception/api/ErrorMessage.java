3eaab4cc3d72543e8b312805b156796f9cbe2f13
Venkata Krishna Repalle <rvkrishna13052001@gmail.com>
2024-05-03 19:20:11 +0530

    Exception Handling Improved

--- src/main/java/com/learning/emsmybatisliquibase/exception/api/ErrorMessage.java
+++ src/main/java/com/learning/emsmybatisliquibase/exception/api/ErrorMessage.java
@@ -1,10 +1,9 @@
 package com.learning.emsmybatisliquibase.exception.api;
 
-import java.text.MessageFormat;
 
 public class ErrorMessage {
-    public static String message(String error, String dynamicValue) {
-        return MessageFormat.format(error, dynamicValue);
+
+    private ErrorMessage() {
     }
 
     public static ErrorResponse errorResponse(String error, String dynamicValue) {
@@ -13,20 +12,6 @@ public class ErrorMessage {
                 .error(Error.builder()
                         .code(error)
                         .message(dynamicValue)
-                        .build())
-                .build();
-    }
-
-    public static ErrorResponse errorResponse(String error) {
-        String[] errorCodeAndMessage = error.split("-", 2);
-        String errorCode = errorCodeAndMessage[0];
-        String errorMessage = errorCodeAndMessage[1];
-
-        return ErrorResponse.builder()
-                .status(StatusType.ERROR)
-                .error(Error.builder()
-                        .code(errorCode)
-                        .message(errorMessage)
                         .build())
                 .build();
     }
