2f839fef9d77bf7d7a029d36e1a640c0ecd6a509
repalle venkata krishna <rvkrishna13052001@gmail.com>
2024-10-20 19:34:49 +0530

    Cors Config updated

--- src/main/java/com/learning/emsmybatisliquibase/config/OpenApiConfig.java
+++ src/main/java/com/learning/emsmybatisliquibase/config/OpenApiConfig.java
@@ -10,41 +10,46 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
-    info = @Info(
-        title = "Employee Management System API",
-        description = "EMS API",
-        summary = "API functionalities for EMS",
-        termsOfService = "T&C",
-        contact = @Contact(
-            name = "Venkata Krishna Repalle",
-            email = "rvkrishna13052001@gmail.com"
-        ),
-        license = @License(
-            name = "abcd"
-        ),
-        version = "v3"
+        info = @Info(
+                title = "Employee Management System API",
+                description = "EMS API",
+                summary = "API functionalities for EMS",
+                termsOfService = "T&C",
+                contact = @Contact(
+                        name = "Venkata Krishna Repalle",
+                        email = "rvkrishna13052001@gmail.com"
+                ),
+                license = @License(
+                        name = "abcd"
+                ),
+                version = "v3"
 
-    ),
-    servers = {
-        @Server(
-            description = "Dev",
-            url = "http://localhost:8080"
         ),
-        @Server(
-            description = "Test",
-            url = "http://localhost:8080"
+        servers = {
+                @Server(
+                        description = "Dev",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Test",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Production",
+                        url = "https://emssopra.azurewebsites.net"
+
+                )
+        },
+        security = @SecurityRequirement(
+                name = "JWT"
         )
-    },
-    security = @SecurityRequirement(
-        name = "JWT"
-    )
 )
 
 @SecurityScheme(
-    name = "JWT",
-    type = SecuritySchemeType.HTTP,
-    scheme = "bearer",
-    bearerFormat = "JWT"
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class OpenApiConfig {
 
