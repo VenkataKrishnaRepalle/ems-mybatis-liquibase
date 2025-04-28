2f371ddb047cdf00fa5f11101b9c956f3a7a97fa
repalle venkata krishna <rvkrishna13052001@gmail.com>
2025-04-28 17:30:44 +0530

    attendance changes

--- src/main/java/com/learning/emsmybatisliquibase/controller/AttendanceController.java
+++ src/main/java/com/learning/emsmybatisliquibase/controller/AttendanceController.java
@@ -1,15 +1,14 @@
 package com.learning.emsmybatisliquibase.controller;
 
-
 import com.learning.emsmybatisliquibase.dto.ApplyAttendanceDto;
 import com.learning.emsmybatisliquibase.dto.UpdateAttendanceDto;
 import com.learning.emsmybatisliquibase.dto.ViewEmployeeAttendanceDto;
 import com.learning.emsmybatisliquibase.entity.Attendance;
 import com.learning.emsmybatisliquibase.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +27,7 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
+    @PreAuthorize("hasAnyRole('EMPLOYEE', 'MANAGER', 'ADMIN')")
     @PostMapping("/apply/{employeeUuid}")
     public ResponseEntity<List<Attendance>> apply(@PathVariable UUID employeeUuid, @RequestBody
     List<ApplyAttendanceDto> attendanceDto) {
@@ -35,30 +35,36 @@ public class AttendanceController {
                 HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('EMPLOYEE', 'MANAGER', 'ADMIN')")
     @PutMapping("/update/{employeeUuid}/attendance/{attendanceUuid}")
     public ResponseEntity<Attendance> update(@PathVariable UUID employeeUuid, @PathVariable UUID attendanceUuid,
                                              @RequestBody UpdateAttendanceDto attendanceDto) {
         return new ResponseEntity<>(attendanceService.update(employeeUuid, attendanceUuid, attendanceDto),
                 HttpStatus.ACCEPTED);
     }
+
+    @PreAuthorize("hasAnyRole('EMPLOYEE', 'MANAGER', 'ADMIN')")
     @GetMapping("/get/{employeeUuid}/attendance/{attendanceUuid}")
     public ResponseEntity<Attendance> get(@PathVariable UUID employeeUuid, @PathVariable UUID attendanceUuid) {
         return new ResponseEntity<>(attendanceService.getByUuid(employeeUuid, attendanceUuid),
                 HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('EMPLOYEE', 'MANAGER', 'ADMIN')")
     @GetMapping("/get/{employeeUuid}")
     public ResponseEntity<ViewEmployeeAttendanceDto> getEmployeeAttendance(@PathVariable UUID employeeUuid) {
         return new ResponseEntity<>(attendanceService.getEmployeeAttendance(employeeUuid),
                 HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('EMPLOYEE', 'MANAGER', 'ADMIN')")
     @GetMapping("/get/attendance/manager/{managerUuid}")
     public ResponseEntity<List<ViewEmployeeAttendanceDto>> getAllEmployeesAttendanceByManager(@PathVariable UUID managerUuid) {
         return new ResponseEntity<>(attendanceService.getAllEmployeesAttendanceByManager(managerUuid),
                 HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('EMPLOYEE', 'MANAGER', 'ADMIN')")
     @GetMapping("/get/attendance/full-team/{employeeUuid}")
     public ResponseEntity<List<ViewEmployeeAttendanceDto>> getFullTeamAttendance(@PathVariable UUID employeeUuid) {
         return new ResponseEntity<>(attendanceService.getFullTeamAttendance(employeeUuid),
